e4515efcd80a73491b30e0aee59af7dd4d2a0b76
Marcel <marcelnedza2000@gmail.com>
2023-07-23 15:52:10 +0200

    handling author name in settings added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// New task / New Task Modal
+// New task - variables
 const newTaskBtn = document.querySelector('.category__new-task');
 const newTaskShadow = document.querySelector('.modal-new-task-shadow');
 const newTaskModal = document.querySelector('.modal-new-task');
@@ -10,7 +10,7 @@ const taskDescription = document.querySelector('#task-description');
 const importantCheckbox = document.querySelector('#important-checkbox');
 const completedCheckbox = document.querySelector('#completed-checkbox');
 
-
+// New Task - functions
 const openNewTaskModal = () => {
     newTaskShadow.style.display = 'block';
     newTaskModal.style.display = 'block';
@@ -29,11 +29,55 @@ const closeNewTaskModal = () => {
     cleanNewTaskModalInputs();
 }
 
-const saveNewTaskModal = () => {
+const addTask = () => {
     closeNewTaskModal();
     cleanNewTaskModalInputs();
 }
 
+// Settings - variables
+const openSettingsBtn = document.querySelector('.header__gear-icon');
+const settingsShadow = document.querySelector('.modal-settings-shadow');
+const settingsModal = document.querySelector('.modal-settings');
+const authorInput = document.querySelector('.settings-author');
+const saveSettingsBtn = document.querySelector('.modal-settings-save-btn');
+const closeSettingsBtn = document.querySelector('.modal-settings-close-btn');
+const actualAuthor = document.querySelector('.modal-settings__author');
+
+// Settings - functions
+const openSettingsModal = () => {
+    settingsShadow.style.display = 'block';
+    settingsModal.style.display = 'block';
+}
+
+const cleanSettingsModalInputs = () => {
+    authorInput.value = '';
+}
+
+const closeSettingsModal = () => {
+    settingsShadow.style.display = 'none';
+    settingsModal.style.display = 'none';
+    cleanSettingsModalInputs();
+}
+
+const saveSettings = () => {
+
+    if (authorInput.value === '') {
+        actualAuthor.textContent = actualAuthor.textContent;
+    } else {
+        actualAuthor.textContent = authorInput.value;
+    }
+    closeSettingsModal();
+
+}
+
+
+// New Task
 newTaskBtn.addEventListener('click', openNewTaskModal);
 closeTaskBtn.addEventListener('click', closeNewTaskModal);
-saveTaskBtn.addEventListener('click', saveNewTaskModal);
+saveTaskBtn.addEventListener('click', addTask);
+
+// Settings
+
+openSettingsBtn.addEventListener('click', openSettingsModal);
+saveSettingsBtn.addEventListener('click', saveSettings);
+closeSettingsBtn.addEventListener('click', closeSettingsModal);
