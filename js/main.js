8b88b97f662c014b9d9f5adaeaa3548e3cfcb988
Marcel <marcelnedza2000@gmail.com>
2023-07-23 16:12:05 +0200

    starting working on adding new task

--- js/main.js
+++ js/main.js
@@ -4,11 +4,21 @@ const newTaskShadow = document.querySelector('.modal-new-task-shadow');
 const newTaskModal = document.querySelector('.modal-new-task');
 const saveTaskBtn = document.querySelector('.new-task-save-btn');
 const closeTaskBtn = document.querySelector('.new-task-close-btn');
-
 const taskName = document.querySelector('#task-name');
 const taskDescription = document.querySelector('#task-description');
 const importantCheckbox = document.querySelector('#important-checkbox');
 const completedCheckbox = document.querySelector('#completed-checkbox');
+const tasksBox = document.querySelector('.tasks-box');
+
+
+// Settings - variables
+const openSettingsBtn = document.querySelector('.header__gear-icon');
+const settingsShadow = document.querySelector('.modal-settings-shadow');
+const settingsModal = document.querySelector('.modal-settings');
+const authorInput = document.querySelector('.settings-author');
+const saveSettingsBtn = document.querySelector('.modal-settings-save-btn');
+const closeSettingsBtn = document.querySelector('.modal-settings-close-btn');
+const actualAuthor = document.querySelector('.modal-settings__author');
 
 // New Task - functions
 const openNewTaskModal = () => {
@@ -29,19 +39,28 @@ const closeNewTaskModal = () => {
     cleanNewTaskModalInputs();
 }
 
-const addTask = () => {
+const addNewTask = () => {
+
+    taskName.textContent = taskName.value;
+    taskDescription.textContent = taskDescription.value;
+
+    const newTask = document.createElement('div');
+    newTask.classList.add('task');
+
+    newTask.innerHTML = `
+    <div class="task__status-box"><i class="fa-regular fa-square task__icon"></i></div>
+    <div class="task__info">
+        <p class="task__date">January 25th 2023 04:25PM</p>
+        <p class="task__name">${taskName.textContent}</p>
+        <p class="task__description">${taskDescription.textContent}</p>
+        <p class="task__author">${actualAuthor.textContent}</p>
+    </div>
+    `
+    tasksBox.appendChild(newTask);
+
     closeNewTaskModal();
     cleanNewTaskModalInputs();
 }
-
-// Settings - variables
-const openSettingsBtn = document.querySelector('.header__gear-icon');
-const settingsShadow = document.querySelector('.modal-settings-shadow');
-const settingsModal = document.querySelector('.modal-settings');
-const authorInput = document.querySelector('.settings-author');
-const saveSettingsBtn = document.querySelector('.modal-settings-save-btn');
-const closeSettingsBtn = document.querySelector('.modal-settings-close-btn');
-const actualAuthor = document.querySelector('.modal-settings__author');
 
 // Settings - functions
 const openSettingsModal = () => {
@@ -67,17 +86,14 @@ const saveSettings = () => {
         actualAuthor.textContent = authorInput.value;
     }
     closeSettingsModal();
-
 }
-
 
 // New Task
 newTaskBtn.addEventListener('click', openNewTaskModal);
 closeTaskBtn.addEventListener('click', closeNewTaskModal);
-saveTaskBtn.addEventListener('click', addTask);
+saveTaskBtn.addEventListener('click', addNewTask);
 
 // Settings
-
 openSettingsBtn.addEventListener('click', openSettingsModal);
 saveSettingsBtn.addEventListener('click', saveSettings);
 closeSettingsBtn.addEventListener('click', closeSettingsModal);
